7d0f3df581d8ec4ff58d57d8a6c9ebfa5c3166cb
thitiwat-buatip <subin2538@gmail.com>
2017-03-21 04:24:54 +0700

    Initial Commit

---
+++ Project SE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project SE
+//
+//  Created by thitiwat on 21/3/60.
+//  Copyright © พ.ศ. 2560 thitiwat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
